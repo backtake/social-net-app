161959916b926fc93c2b98866a382cb8775bd13c
backtake <stasiusoltysik@gmail.com>
2018-05-18 15:05:03 +0200

    Add twit and user model to repo

--- src/main/java/com/codechallange/socialnetapp/twit/Twit.java
+++ src/main/java/com/codechallange/socialnetapp/twit/Twit.java
@@ -1,4 +1,29 @@
 package com.codechallange.socialnetapp.twit;
 
+import com.codechallange.socialnetapp.user.User;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
 public class Twit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(TemporalType.DATE)
+    private Calendar date;
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    protected Twit() {}
+
+    public Twit(Calendar date, String message, User user) {
+        this.date = date;
+        this.message = message;
+        this.user = user;
+    }
 }
--- src/main/java/com/codechallange/socialnetapp/user/User.java
+++ src/main/java/com/codechallange/socialnetapp/user/User.java
@@ -1,4 +1,28 @@
 package com.codechallange.socialnetapp.user;
 
+
+import com.codechallange.socialnetapp.twit.Twit;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Twit> twits;
+
+    @OneToMany
+    private Set<User> followed;
+
+    protected User() {}
+
+    public User(String name) {
+        this.name = name;
+    }
 }
