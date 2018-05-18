fb40fa3c710b0721110a0f79609387db97fcedd1
backtake <stasiusoltysik@gmail.com>
2018-05-18 14:08:09 +0200

    Initial commit

---
+++ src/main/java/com/codechallange/socialnetapp/SocialNetAppApplication.java
@@ -0,0 +1,12 @@
+package com.codechallange.socialnetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetAppApplication.class, args);
+	}
+}
