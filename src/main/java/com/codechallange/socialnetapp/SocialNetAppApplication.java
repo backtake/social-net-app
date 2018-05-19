458ee78aa91128ba8d5a5759297dcb5cdaaee7b0
backtake <stasiusoltysik@gmail.com>
2018-05-19 14:12:08 +0200

    Add demo method for demonstrating/testing purpouse

--- src/main/java/com/codechallange/socialnetapp/SocialNetAppApplication.java
+++ src/main/java/com/codechallange/socialnetapp/SocialNetAppApplication.java
@@ -1,5 +1,6 @@
 package com.codechallange.socialnetapp;
 
+import com.codechallange.socialnetapp.twit.Twit;
 import com.codechallange.socialnetapp.twit.TwitRepository;
 import com.codechallange.socialnetapp.user.User;
 import com.codechallange.socialnetapp.user.UserRepository;
@@ -10,26 +11,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class SocialNetAppApplication {
-
-//	private static final Logger log = LoggerFactory.getLogger(SocialNetAppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetAppApplication.class, args);
 	}
 
-//	@Bean
-//    public CommandLineRunner demo(UserRepository userRepository, TwitRepository twitRepository) {
-//	    return (args) -> {
-//	        userRepository.save(new User("Gienek"));
-//	        userRepository.save(new User("Staszek"));
-//	        userRepository.save(new User("Wacek"));
-//
-//	        log.info("Users found with findAll(): ");
-//	        for(User user: userRepository.findAll()) {
-//	            log.info(user.toString());
-//            }
-//        };
-//    }
+	@Bean
+    public CommandLineRunner demo(UserRepository userRepository, TwitRepository twitRepository) {
+	    return (args) -> {
+	        User user1 = new User("Han");
+	        User user2 = new User("Dan");
+	        User user3 = new User("Jan");
+	        userRepository.save(user1);
+	        userRepository.save(user2);
+	        userRepository.save(user3);
+            Twit twit1 = new Twit("wiadomosc1");
+            Twit twit2 = new Twit("wiadomosc2");
+            Twit twit3 = new Twit("wiadomosc3");
+            Twit twit4 = new Twit("wiadomosc4");
+            twit1.setUser(user1);
+            twit2.setUser(user2);
+            twit3.setUser(user3);
+            twit4.setUser(user2);
+            twitRepository.save(twit1);
+            twitRepository.save(twit2);
+            twitRepository.save(twit3);
+            twitRepository.save(twit4);
+            Set<User> followed = new HashSet<>();
+            followed.add(user2);
+            followed.add(user3);
+            user1.setFollowed(followed);
+            userRepository.save(user1);
+
+        };
+    }
 }
