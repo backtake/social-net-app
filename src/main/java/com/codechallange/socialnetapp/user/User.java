00a0e474150d75edc900ef018a2a8e4c19caff5a
backtake <stasiusoltysik@gmail.com>
2018-05-19 13:00:14 +0200

    Remove dead code from the class

--- src/main/java/com/codechallange/socialnetapp/user/User.java
+++ src/main/java/com/codechallange/socialnetapp/user/User.java
@@ -1,10 +1,6 @@
 package com.codechallange.socialnetapp.user;
 
-
-import com.codechallange.socialnetapp.twit.Twit;
-
 import javax.persistence.*;
-import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -15,11 +11,8 @@ public class User {
     private Long id;
     private String name;
 
-//    @OneToMany
-//    private List<Twit> twits;
-
-//    @OneToMany
-//    private Set<User> followed;
+    @OneToMany
+    private Set<User> followed;
 
     public void setName(String name) {
         this.name = name;
@@ -37,19 +30,11 @@ public class User {
         this.id = id;
     }
 
-//    public List<Twit> getTwits() {
-//        return this.twits;
-//    }
-//
-//    public void setTwits(List<Twit> twits) {
-//        this.twits = twits;
-//    }
+    public Set<User> getFollowed() {
+        return followed;
+    }
 
-//    public Set<User> getFollowed() {
-//        return followed;
-//    }
-//
-//    public void setFollowed(Set<User> followed) {
-//        this.followed = followed;
-//    }
+    public void setFollowed(Set<User> followed) {
+        this.followed = followed;
+    }
 }
